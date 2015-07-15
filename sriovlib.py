0d5b6f6a4a7ed6571ace0b29bb98785655f1dff9
Roman Bogorodskiy <rbogorodskiy@mirantis.com>
2015-07-15 11:37:24 +0200

    Initial import

---
+++ sriovlib.py
@@ -0,0 +1,69 @@
+import os
+import os.path
+
+SYS_PCI_ADDR_PATH = "/sys/bus/pci/devices"
+SYS_IFNAME_PATH = "/sys/class/net/"
+VFPREFIX = "virtfn"
+
+
+def ifname_to_pci_addr(ifname):
+    dev_path = os.path.join(SYS_IFNAME_PATH, ifname)
+
+    if not os.path.exists(dev_path):
+        return None
+
+    pci_dev_link = os.path.join(dev_path, 'device')
+    return os.path.split(os.readlink(pci_dev_link))[1]
+
+
+def pci_addr_to_ifname(pci_addr):
+    pci_path = os.path.join(SYS_PCI_ADDR_PATH, pci_addr)
+
+    if not os.path.exists(pci_path):
+        return None
+
+    net_dev_path = os.path.join(pci_path, 'net')
+    if not os.path.exists(net_dev_path):
+        return None
+
+    items = os.listdir(net_dev_path)
+    if len(items) < 1:
+        return None
+
+    return items[0]
+
+
+def show(ifname):
+    dev_path = os.path.join(SYS_IFNAME_PATH, ifname)
+
+    if not os.path.exists(dev_path):
+        return None
+
+    pci_dev_link = os.path.join(dev_path, 'device')
+
+    info = {}
+    for i in ('device', 'vendor', 'sriov_numvfs'):
+        path = os.path.join(pci_dev_link, i)
+        with open(path, 'r') as f:
+            info[i] = f.read().strip()
+    info['pci_addr'] = os.path.split(os.readlink(pci_dev_link))[1]
+    info['vfs'] = []
+    for i in os.listdir(pci_dev_link):
+        if i.startswith(VFPREFIX):
+            vf_link = os.path.join(pci_dev_link, i)
+            vf_addr = os.path.split(os.readlink(vf_link))[1]
+            info['vfs'].append(vf_addr)
+
+    return info
+
+
+def list():
+    sriov_devs = []
+    devs = os.listdir(SYS_IFNAME_PATH)
+    for dev in devs:
+        dev_path = os.path.join(SYS_IFNAME_PATH, dev,
+                                'device', 'sriov_numvfs')
+        if os.path.exists(dev_path):
+            sriov_devs.append(dev)
+
+    return sriov_devs
---
+++ sriovmng.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import sys
+import types
+
+import click
+
+import sriovlib
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command(short_help='get interface PCI address by its name')
+@click.argument('ifname')
+def ifname_to_pci_addr(ifname):
+    pci_addr = sriovlib.ifname_to_pci_addr(ifname)
+    if not pci_addr:
+        click.echo('No device found: %s' % ifname, err=True)
+        sys.exit(1)
+
+    click.echo("%s: %s" % (ifname, pci_addr))
+
+
+@click.command(short_help='get interface name by its PCI address')
+@click.argument('pci_addr')
+def pci_addr_to_ifname(pci_addr):
+    ifname = sriovlib.pci_addr_to_ifname(pci_addr)
+    if not ifname:
+        click.echo('No corresponding device for %s found' % pci_addr,
+                   err=True)
+        sys.exit(1)
+    click.echo("%s: %s" % (pci_addr, ifname))
+
+
+@click.command(short_help='show details about PF interface')
+@click.argument('ifname')
+def show(ifname):
+    info = sriovlib.show(ifname)
+    for k, v in info.iteritems():
+        if isinstance(v, types.ListType):
+            value = ' '.join(v)
+        else:
+            value = v
+        click.echo("%14s: %s" % (k, value))
+
+
+@click.command(short_help='show list of PF devices')
+def list():
+    devices = sriovlib.list()
+    for device in devices:
+        click.echo("%s" % device)
+
+cli.add_command(ifname_to_pci_addr)
+cli.add_command(pci_addr_to_ifname)
+cli.add_command(show)
+cli.add_command(list)
+
+if "__main__" == __name__:
+    cli()
